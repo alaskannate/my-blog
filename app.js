972b7efb5d0e18e076980fddabbd8ccec8537919
alaskannate <64567768+alaskannate@users.noreply.github.com>
2023-07-18 14:54:11 -0700

    added a login page, and auth functionality

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const date = require(__dirname + "/date.js");
-const mongoose = require("mongoose");
-
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const todaysDate = date.getDate()
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
 
+// Create session 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 // MAIN DATABASE SECTION
 
@@ -51,51 +57,27 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: [true, "a text body is required"],
     minlength: [10, "This text body is too short"],
-    maxlength: [1000, "This text body is too long"],
+    maxlength: [10000, "This text body is too long"],
     trim: true,
   }
 });
 const Post = mongoose.model('Post', postSchema);
 
-//Defining schema for the database collection "user"...
+
+//new user schema.
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "required"],
-    minlength: [2, "Sorry, this name is too short"],
-    maxlength: [25, "Sorry, this name is too long"]
-  },
-  email: {
-    type: String,
-    required: [true, "required"],
-  },
-  authoredPosts: postSchema
+  email: String,
+  password: String,
 });
+
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema)
 
+passport.use(User.createStrategy());
 
-
-// this is one way to delete an item from a databas.
-// Post.deleteOne({ date: "today" })
-// .then(() => {
-//   console.log("successfully deleted the document");
-// })
-// .catch((err) => {
-//   console.log(err);
-// });
-
-
-// "find" collection data and log to the console. check for errors, if any log those. 
-// User.find({})
-//   .then((users) => {
-//     users.forEach((user) => {
-//       console.log(user.name);
-//     });
-//   })
-//   .catch((err) => console.error('Error querying collection:', err))
-//   .finally(() => mongoose.disconnect());
-
-
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -105,6 +87,7 @@ const User = mongoose.model('User', userSchema)
 app.get("/", (req, res) => {
 
   Post.find({})
+  .sort({ _id: -1 })
     .then((foundPosts) => {
       res.render('home', {
         newPost: foundPosts,
@@ -117,38 +100,110 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/login", (req, res) => {
+  res.render("login", );
+});
+
+app.get("/register", (req, res) => {
+  res.render("register")
+})
+
+app.get('/admin', (req, res) => {
+  if (req.isAuthenticated()) {
+      res.render("admin")
+  } else {
+      res.redirect('/login')
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+      console.log(err)
+  });
+  res.redirect('/');
+});
 
 // gets about content
-app.get('/about', (req, res) => {
-  res.render('about', {
+app.get("/about", (req, res) => {
+  res.render("about", {
     aboutContent: aboutContent
   });
 });
 
-app.get('/flashcards', (req, res) => {
+app.get("/flashcards", (req, res) => {
   res.render('flashcards');
 })
 
 // gets contact content 
-app.get('/contact', (req, res) => {
-  res.render('contact', {
+app.get("/contact", (req, res) => {
+  res.render("contact", {
     contactContent: contactContent
   });
 });
 
 //gets compose page where user can create new content. 
-app.get('/compose', (req, res) => {
-  res.render('compose', );
+app.get("/compose", (req, res) => {
+  res.render("compose", );
 });
 
 
-app.get('/login', (req, res) => {
-  res.render('login', );
+
+app.get('/post/:postID', (req, res) => {
+
+  const requestedPost = req.params.postID;
+
+  Post.findById(requestedPost.toString())
+    .then((foundPost) => {
+      res.render('post', {
+        foundPost: foundPost,
+      });
+    })
+    .catch((error) => {
+      console.error('Error Finding items:', error);
+      res.status(500).send('Error retrieving items');
+    });
+});
+
+
+app.post("/register", (req, res) => {
+
+  User.register({
+      username: req.body.username
+  }, req.body.password, (err, user) => {
+      if (err) {
+          console.log(err);
+          res.redirect('/register');
+      } else {
+          passport.authenticate('local')(req, res, () => {
+              res.redirect('/admin');
+          })
+      }
+  })
+});
+
+app.post("/login", (req, res) => {
+
+  const user = new User({
+      username: req.body.username,
+      password: req.body.password
+  });
+
+  req.login(user, (err) => {
+      if (err) {
+          console.log(err);
+      } else {
+          passport.authenticate('local')(req, res, () => {
+              res.redirect('/admin');
+          })
+      };
+
+  });
+
 });
 
 app.post("/compose", async (req, res) => {
 
-  const postDate = todaysDate;
+  const postDate = date.getDate();
   const postTitle = req.body.postTitle;
   const postBody = req.body.postBody;
 
@@ -169,21 +224,7 @@ app.post("/compose", async (req, res) => {
 });
 
 
-app.get('/post/:postID', (req, res) => {
 
-  const requestedPost = req.params.postID;
-
-  Post.findById(requestedPost.toString())
-    .then((foundPost) => {
-      res.render('post', {
-        foundPost: foundPost,
-      });
-    })
-    .catch((error) => {
-      console.error('Error Finding items:', error);
-      res.status(500).send('Error retrieving items');
-    });
-});
 
 
 
@@ -195,4 +236,3 @@ if (port == null || port == "") {
   console.log("Server started on port 3000");
 }
 app.listen(port);
-
--- public/script.js
+++
@@ -1,5 +0,0 @@
-const paragraph = document.getElementById("paragraph");
-
-paragraph.addEventListener("click", ()=> {
-  paragraph.classList.toggle("expanded");
-});
